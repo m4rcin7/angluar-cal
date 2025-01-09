3875cd58aa1292582a2352a6f99a4932a97f2aa3
mkpage1337 <mrku1337@gmail.com>
2025-01-09 15:41:13 +0100

    commit

--- src/app/investment-results/investment-results.ts
+++ src/app/investment-results/investment-results.ts
@@ -1,26 +1,14 @@
-// Use the below code as a help
-// e.g., integrate it into a service or component
-// You may need to tweak it, depending on where and how you use it
+import { Component, computed, inject } from '@angular/core';
 
-function calculateInvestmentResults() {
-  const annualData = [];
-  let investmentValue = initialInvestment;
+import { InvestmentService } from '../investment.service';
 
-  for (let i = 0; i < duration; i++) {
-    const year = i + 1;
-    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-    investmentValue += interestEarnedInYear + annualInvestment;
-    const totalInterest =
-      investmentValue - annualInvestment * year - initialInvestment;
-    annualData.push({
-      year: year,
-      interest: interestEarnedInYear,
-      valueEndOfYear: investmentValue,
-      annualInvestment: annualInvestment,
-      totalInterest: totalInterest,
-      totalAmountInvested: initialInvestment + annualInvestment * year,
-    });
-  }
+@Component({
+  selector: 'app-investment-results',
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
 
-  return annualData;
+  results = computed(() => this.investmentService.resultData());
 }
